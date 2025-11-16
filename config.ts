2cc2aa9b69e033c01f6fe718f1ec77984b697ce9
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-11-16 19:29:57 +0530

    1o2j

--- config.ts
+++ config.ts
@@ -1,9 +1,11 @@
 import * as admin from "firebase-admin";
 import serviceAccount from "./public/serviceAccountKey.json";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    databaseURL: "https://anony-4266e-default-rtdb.firebaseio.com"
+    databaseURL: process.env.FIREBASE_DATABASE_URL!,
 });
 
 const db: admin.database.Database = admin.database();
