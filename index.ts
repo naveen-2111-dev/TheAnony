d8fd24ad7c71240949a503c279d4357416094a95
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-11-18 16:41:44 +0530

    221dw

--- index.ts
+++ index.ts
@@ -2,8 +2,20 @@ import express from "express";
 import serverless from "serverless-http";
 import app from "./src/app";
 
+import { configDotenv } from "dotenv";
+
+configDotenv();
+
 const expressApp = express();
 expressApp.use("/", app);
-
 export const handler = serverless(expressApp);
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== "production") {
+    expressApp.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
 export default handler;
